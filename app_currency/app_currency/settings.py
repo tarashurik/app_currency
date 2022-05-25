b1a1ebdcbb091143c2e348c4ffce80ce1e5eaea5
tarashurik@mail.ru <tarashurik@mail.ru>
2022-05-25 12:10:36 +0300

    add requirements.txt, .env.example, change DEBUG and SECRET_KEY settings, change title in base.html

--- app_currency/app_currency/settings.py
+++ app_currency/app_currency/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bco!+z8y6^jpurnc7h7vp(voy*)$rfa4@$jh-xlbr&4-5h!mpd'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-bco!+z8y6^jpurnc7h7vp(voy*)$rfa4@$jh-xlbr&4-5h!mpd')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = []
 
